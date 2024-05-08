ffd82cdf309b7513618436daacc2bd06e6398952
Aaron-zon <1640485761@qq.com>
2024-05-08 21:52:51 +0800

    webApi drag

--- src/views/006.vue
+++ src/views/006.vue
@@ -1,36 +1,46 @@
 <script lang="ts" setup>
+// https://developer.mozilla.org/zh-CN/docs/Web/API/HTML_Drag_and_Drop_API
+
+/**
+ * 拖拽元素
+ * - dragstart: 拖拽开始
+ * - drag: 拖动
+ * - dragend: 拖拽结束
+ * 放置区域
+ * - dragover: 进入可释放目标
+ * - dragenter: 进入可释放目标(每100毫秒执行一次)
+ * - dragleave: 离开可释放目标
+ * - drop: 释放
+ */
+// * 要触发drop事件必须取消dragover的默认事件，因为dragover事件不允许其他元素在其元素上释放
+// * 触发drop事件时最好也取消默认事件，在内部有文字或链接时可能会有影响
 import { ref, onMounted } from 'vue'
-const box1List = ref([
-    {
-        id: 1,
-        name: 'drag-1'
-    },
-    {
-        id: 2,
-        name: 'drag-2'
-    },
-    {
-        id: 3,
-        name: 'drag-3'
-    },
-])
-const item1 = ref()
+const dr1 = ref()
 const box2 = ref()
 
 onMounted(() => {
-    console.log(item1.value[0])
-    const dom: HTMLElement = item1.value[0]
+    const dragDom = dr1.value
+    const targetDom = box2.value
 
-    dom.addEventListener('dragstart', (e: DragEvent) => {
-        console.log(e)
+    targetDom.addEventListener('dragover', (e) => {
+        e.preventDefault()
+        console.log('dragover')
     })
-    dom.addEventListener('dragend', () => {
-        console.log('dom dragend')
+    targetDom.addEventListener('dragenter', () => console.log('dragenter'))
+    targetDom.addEventListener('dragleave', () => console.log('dragleave'))
+    targetDom.addEventListener('drop', (e) => {
+        e.preventDefault()
+        targetDom.appendChild(dragDom)
+        console.log('drop')
     })
-    const box2Dom: HTMLElement = box2.value
-    box2Dom.addEventListener('dragend', () => {
-        console.log('dragend')
-    })
+
+    // 拖拽开始
+    // dragDom.addEventListener('dragstart', () => console.log('content dragstart'))
+    // // 拖动
+    // dragDom.addEventListener('drag', () => console.log('content drag'))
+    // // 拖拽结束
+    // dragDom.addEventListener('dragend', () => console.log('content dragend'))
+
 })
 
 
@@ -40,8 +50,8 @@ onMounted(() => {
     <Paper>
         <div class="flex">
             <div class="flex flex-col gap-2 p-4 w-300px h-300px m-auto bg-gray-500/5 rounded b-solid b-c-#000">
-                <div v-for="item in box1List" class="cursor-move h-30 bg-gray-500/5 rounded p-3 cursor-move" :ref="`item${item.id}`" draggable="true">
-                    {{ item.name }}
+                <div ref="dr1" class="cursor-move h-30 bg-gray-500/5 rounded p-3 cursor-move" draggable="true">
+                    被拖拽元素
                 </div>
             </div>
 
